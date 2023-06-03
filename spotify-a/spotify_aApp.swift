70d9c1c652b6d83c0fa29f41a3b907fec74ee2ef
Javier Aponte <31341776+javier-aponte@users.noreply.github.com>
2023-06-03 11:34:54 -0500

    first commit

---
+++ spotify-a/spotify_aApp.swift
@@ -0,0 +1,29 @@
+//
+//  spotify_aApp.swift
+//  spotify-a
+//
+//  Created by Linder Anderson Hassinger Solano    on 26/05/23.
+//
+
+import SwiftUI
+
+@main
+struct spotify_aApp: App {
+    
+    @StateObject var appState: AppState = .shared
+    var body: some Scene {
+        WindowGroup {
+            switch appState.currentScreen {
+            case .launch:
+                LaunchView()
+                    .environmentObject(appState)
+            case .signIn:
+                MainView()
+                    .environmentObject(appState)
+            case .main:
+                MainView()
+                    .environmentObject(appState)
+            }
+        }
+    }
+}
